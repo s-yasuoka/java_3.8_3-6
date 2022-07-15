f9e16b6601f6a37daf3b7c618cb827ce96997349
s-yasuoka <migayasu@gmail.com>
2022-07-15 13:26:23 +0900

    miain

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main{
+  public static void main(String[] args){
+    // ①
+    weight == 60
+    // ②
+    (age1 + age2) * 2 >60
+    // ③
+    age % 2 == 1
+    // ④
+    name.equals ("湊")
+  }
+}
